0683ed1032a57056fadf45c56d2a2d6e815092df
GeorgeHN666 <jorge1029hsgmail.com>
2021-10-20 05:31:56 +0200

    .

--- 002/main.go
+++ 002/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/GeorgeHN666/blockchain/blockchain"
 )
 
 func main() {
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block After Genesis")
 	chain.AddBlock("Second block After Genesis")
@@ -15,5 +18,9 @@ func main() {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
